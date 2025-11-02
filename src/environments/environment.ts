338d52b6424b0434f42bde17b797ff16e0159c59
selva <selvaselvamurugan193@gmail.com>
2025-11-02 23:49:19 +0530

    production configuration update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,13 @@
 export const environment = {
     production: true,
-    
+    apiUrl: "http://51.21.249.41/api",
+    firebaseConfig: {
+        apiKey: "AIzaSyAFt7ig7yETMJlixYGxz_yaA5euuZFoy70",
+        authDomain: "loopin-a446c.firebaseapp.com",
+        projectId: "loopin-a446c",
+        storageBucket: "loopin-a446c.firebasestorage.app",
+        messagingSenderId: "1048832349921",
+        appId: "1:1048832349921:web:4709bd37e1281b997909a0",
+        measurementId: "G-Q517M5V921"
+    }
 };
